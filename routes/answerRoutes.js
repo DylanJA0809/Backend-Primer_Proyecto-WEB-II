36666999efe60691dc4ef1404709a74ea194d29d
emilyzuniga049 <emilyzuniga049@gmail.com>
2026-03-17 16:34:23 -1200

    Add routes for answer and question

---
+++ routes/answerRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const { answerPost, answerGet } = require("../controllers/answerController");
+const authenticateToken = require("../controllers/authJWT");
+
+router.post("/answer", authenticateToken, answerPost);
+
+router.get("/answer", answerGet);
+
+module.exports = router;
---
+++ routes/questionRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const { questionPost, questionGet } = require("../controllers/questionController");
+const authenticateToken = require("../controllers/authJWT");
+
+router.post("/question", authenticateToken, questionPost);
+
+router.get("/question", questionGet);
+
+module.exports = router;
