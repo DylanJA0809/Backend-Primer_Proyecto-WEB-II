de20d51b39b16c2aef524adf2b31dc3dd48fa7c1
Dylan J.A <dylanjimenezalfaro.ml@gmail.com>
2026-03-18 01:58:20 -0600

    Ajustes en questionRoutes

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { questionPost, questionGet } = require("../controllers/questionController");
-const authenticateToken = require("../controllers/authJWT");
+const { authenticateToken } = require("../controllers/authJWT");
+
 
 router.post("/question", authenticateToken, questionPost);
 
