a5b1fcee24879eb76c9a5b8e23ee12b6a84945bf
Dylan J.A <dylanjimenezalfaro.ml@gmail.com>
2026-03-18 01:58:06 -0600

    Correcciones en answerRoutes

--- routes/answerRoutes.js
+++ routes/answerRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { answerPost, answerGet } = require("../controllers/answerController");
-const authenticateToken = require("../controllers/authJWT");
+const { authenticateToken } = require("../controllers/authJWT");
+
 
 router.post("/answer", authenticateToken, answerPost);
 
